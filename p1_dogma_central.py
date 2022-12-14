b766a079fabecfa1f7c8209fd5a9b12ba0ed4f63
Ramon Cruz <ramoncruz@ciencias.unam.mx>
2022-12-14 15:50:57 -0600

    conteo de subcadenas

--- p1_dogma_central.py
+++ p1_dogma_central.py
@@ -7,7 +7,7 @@ Marta Yunnuen Pacheco -
 Oscar Emilio Caballero Jimenez - 
 
 '''
-
+import numpy as np
 def leerFASTA(pathArchivo):
     archivo = open(pathArchivo, "r")
     cabereza = archivo.readline()
@@ -26,7 +26,6 @@ def LCS(X, Y):
  
     # `lookup[i][j]` almacena la longitud de LCS de la subcadena `X[0…i-1]` e `Y[0…j-1]`
     lookup = [[0 for x in range(n + 1)] for y in range(m + 1)]
- 
     # llenar la tabla de búsqueda de forma ascendente
     for i in range(1, m + 1):
         for j in range(1, n + 1):
@@ -34,25 +33,27 @@ def LCS(X, Y):
             # si el carácter actual de `X` e `Y` coincide
             if X[i - 1] == Y[j - 1]:
                 lookup[i][j] = lookup[i - 1][j - 1] + 1
- 
+                
                 # actualiza la longitud máxima y el índice final
                 if lookup[i][j] > maxLength:
                     maxLength = lookup[i][j]
                     endingIndex = i
  
     # devuelve la subcadena común más larga que tiene una longitud `maxLength`
-    return X[endingIndex - maxLength: endingIndex]
+    return X[endingIndex - maxLength: endingIndex],np.count_nonzero(np.array(lookup) > 2)
 
 humanh11 = leerFASTA('./humanh11.txt')
 humanh12 = leerFASTA('./humanh12.txt')
 macaca11 = leerFASTA('./macacah11.txt')
 macaca12 = leerFASTA('./macacah12.txt')
 
+
 print('--- GENES PARALOGOS ---')
 print('')
 print('Human histone h1.1: ', humanh11)
 print('Human histone h1.2: ', humanh12)
 print('Cadena común más larga: ', LCS(humanh11, humanh12))
+
 print('')
 print('Macaca fascicularis histone h1.1: ', macaca11)
 print('Macaca fascicularis histone h1.2: ', macaca12)
